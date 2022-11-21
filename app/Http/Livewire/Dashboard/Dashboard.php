a0f4ce761c040b4f84733f0531e055056e1fe3dc
elmedinakqiku1 <elmedinakqiku@gmail.com>
2022-11-21 16:52:07 +0100

    dashboard

--- app/Http/Livewire/Dashboard/Dashboard.php
+++ app/Http/Livewire/Dashboard/Dashboard.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire\Dashboard;
 
 use App\Models\Category;
 use App\Models\Post;
+
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,23 +13,32 @@ class Dashboard extends Component
 {
     use WithPagination;
 
-    public $posts;
-    public $categories;
+    public ?int $categoryId = null;
+
+    protected $queryString = [
+        'categoryId'
+    ];
 
     public function render()
     {
         $posts = $this->getPosts();
         $categories = $this->getCategories();
+
         return view('livewire.dashboard.dashboard', compact('posts', 'categories'));
     }
 
     public function getPosts()
     {
-        return Post::paginate(8);
+        return Post::query()
+            ->when(
+                $this->categoryId,
+                fn(Builder $builder) => $builder->whereRelation('categories', 'categories.id', $this->categoryId)
+            )
+            ->paginate(8);
     }
 
     public function getCategories()
     {
-        return Category::all();
+        return Category::query()->get();
     }
 }
